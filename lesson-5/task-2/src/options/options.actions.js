a9b541a9d46362c52cab17c6abf3f6c7e036c452
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2023-02-05 09:36:50 +0200

    lesson 5

---
+++ lesson-5/task-2/src/options/options.actions.js
@@ -0,0 +1,12 @@
+export const TOGGLE_OPTION = 'OPTIONS/TOGGLE_OPTION';
+
+const toggleOption = optionId => {
+  return {
+    type: TOGGLE_OPTION,
+    payload: {
+      optionId,
+    },
+  };
+};
+
+export default toggleOption;
